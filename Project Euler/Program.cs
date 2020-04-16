b99d909ce14d38e6ec614ef12a7618f86449555c
xtremealu <samardzija.robert@gmail.com>
2020-04-16 17:58:37 +0200

    Finish solutions 18, 19 and clean up code.

--- Project Euler/Program.cs
+++ Project Euler/Program.cs
@@ -6,7 +6,7 @@ namespace Project_Euler
     {
         private static void Main()
         {
-            Console.WriteLine(P017.Solve());
+            Console.WriteLine(P019.Solve());
         }
     }
 }
